a6f4affdfbf57df5bee36ba25a6dedeb8aad7eaf
SkrilleX <69858510+IND-Syntax-Error@users.noreply.github.com>
2021-04-04 06:32:56 +0530

    Add files via upload

---
+++ even_evensquared.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class even_evensquared {
+	double n;
+	double sn;
+	public even_evensquared()
+	{
+		n = 0;    
+	}
+	public void accept()
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter n : ");
+		n = input.nextDouble();
+		input.close();
+	}
+	public void eNum()
+	{
+		for (int i = 1; i <= n; i+= 2)
+		{
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+	public void eNumSquare()
+	{
+		for (int i = 0; i <= n; i += 2)
+		{
+			sn = Math.pow(i, 2);
+			System.out.println(sn + " ");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		even_evensquared ees = new even_evensquared();
+		ees.accept();
+		ees.eNum();
+		ees.eNumSquare();
+	}
+
+}
